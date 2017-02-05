801be4372e18845af4405e81a1cbc9161b1a1980
Y <Y@DESKTOP-82K8V5U>
2017-02-05 21:32:43 +0800

    Add project files.

---
+++ Maya.Model/Assassin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maya.Model
+{
+    public class Assassin : Maya.Common.Classes.IAssassin
+    {
+
+    }
+}
